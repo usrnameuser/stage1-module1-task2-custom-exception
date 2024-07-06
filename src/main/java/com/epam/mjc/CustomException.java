bb12e891037899a88d0f4200670e31e855885335
usrnameuser <150504703+usrnameuser@users.noreply.github.com>
2024-07-06 13:16:10 +0300

    Update CustomException.java

--- src/main/java/com/epam/mjc/CustomException.java
+++ src/main/java/com/epam/mjc/CustomException.java
@@ -3,7 +3,7 @@ package com.epam.mjc;
 import java.lang.IllegalArgumentException;
 
 public class CustomException extends IllegalArgumentException {
-      public CustomException(String id) {
+      public CustomException(long id) {
         super("Could not find student with ID {" + id + "}");
     }
 }

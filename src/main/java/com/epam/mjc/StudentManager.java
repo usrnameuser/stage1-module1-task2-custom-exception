1612cac2a8cdf5b0b70bd068cf2a46d5fdf37df9
usrnameuser <150504703+usrnameuser@users.noreply.github.com>
2024-07-06 18:12:44 +0300

    Update StudentManager.java

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -6,12 +6,14 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {    
-    try {
-      return Student.getValueOf(studentID);
+  public Student find(long studentID) {
+    Student student;
+    try {  
+      student = Student.getValueOf(studentID);
     } catch(IllegalArgumentException illegalArgumentException) {
         throw new CustomException(studentID);
     }
+    return student;
   }
 
   public static void main(String[] args) {
